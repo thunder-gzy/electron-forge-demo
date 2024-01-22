ba6d7353407133d8297556f6de6a637615ab2160
guziyi <a1725137488@gmail.com>
2024-01-22 10:32:43 +0800

    error: Network Error

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
+const { app, BrowserWindow, ipcMain, session } = require('electron')
 const path = require('node:path')
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -10,10 +10,26 @@ const createWindow = () => {
     },
   })
 
-  win.loadFile('index.html')
+  win.loadFile('dist/index.html')
 }
 
 app.whenReady().then(() => {
+  session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
+    callback({
+      responseHeaders: {
+        ...details.responseHeaders,
+        'Content-Security-Policy': '*', // 允许所有的资源加载
+        'Access-Control-Allow-Origin': '*', // 允许跨域
+        'Cross-Origin-Embedder-Policy': ['require-corp'], // 允许跨域
+        'Cross-Origin-Opener-Policy': ['same-origin'], // 允许跨域
+      }
+    })
+  })
+  session.defaultSession.setProxy({
+    mode: 'fixed_servers',
+    proxyRules: 'http://111.198.15.161:9900'
+  })
+
   createWindow()
   ipcMain.handle('ping', () => 'pong')
   app.on('activate', () => {
